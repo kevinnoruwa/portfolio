b7e8e5dc9b79b52139913097b267b40c021905c9
The-Unknown-Developer <theunknowndevloper@gmail.com>
2021-06-22 19:56:54 -0400

    added google tracking also added new jumbo video screen

--- app.js
+++ app.js
@@ -28,3 +28,22 @@ menuBtn[0].addEventListener('click', clickedBtn)
 
 
 exitMenu[0].addEventListener('click', clickedExit)
+
+
+let topTl = gsap.timeline()
+
+topTl.fromTo (
+    "#top .heading .name",
+    {
+        opacity: 0,
+
+    },
+    {
+        opacity: 1,
+        duration: 4.5
+
+    }
+
+
+
+)
